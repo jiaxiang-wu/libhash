301609b9de85dda59dbc929fd6f9a8a3d11405ec
Jiaxiang Wu <jiaxiang.wu.90@gmail.com>
2017-01-20 14:01:37 +0800

    use the original compressSH() routine as the hashing function

--- util/TrnHashMdl_SH.m
+++ util/TrnHashMdl_SH.m
@@ -13,12 +13,12 @@ fprintf('[INFO] entering TrnHashMdl_SH()\n');
 % add path for all files under ./extern/spectral_hashing/util
 addpath(genpath('./extern/spectral_hashing'));
 
-% learn parameters of spectral hashing
+% call <trainSH> to obtain SH parameters
 param.nbits = paraStr.hashBitCnt;
 param = trainSH(double(dataMat'), param);
 
 % create the hashing function handler
-model.hashFunc = @(dataMat)(HashFuncImpl(dataMat', param)');
+model.hashFunc = @(dataMat)(HashFuncImpl(dataMat, param));
 
 end
 
@@ -26,22 +26,13 @@ function codeMat = HashFuncImpl(dataMat, param)
 % INTRO
 %   calculate binary codes with pre-trained SH parameters
 % INPUT
-%   dataMat: N x D (data matrix)
+%   dataMat: D x N (data matrix)
 %   param: struct (pre-trained SH parameters)
 % OUTPUT
-%   codeMat: N x R (binary code matrix)
+%   codeMat: R x N (binary code matrix)
 
-% project the data matrix via PCA
-dataMat = bsxfun(@minus, dataMat * param.pc, param.mn);
-omegMat = bsxfun(@times, param.modes, pi ./ (param.mx - param.mn));
-
-% calculate the binary code matrix
-smplCnt = size(dataMat, 1);
-codeMat = zeros(smplCnt, param.nbits);
-for bitIdx = 1 : param.nbits
-  ys = sin(bsxfun(@times, dataMat, omegMat(bitIdx, :)) + pi / 2);
-  yi = prod(ys, 2);
-  codeMat(:, bitIdx) = (yi > 0) * 2 - 1;
-end
+% call <compressSH> to calculate binary codes
+[~, codeMat] = compressSH(dataMat', param);
+codeMat = (codeMat' > 0) * 2 - 1;
 
 end
